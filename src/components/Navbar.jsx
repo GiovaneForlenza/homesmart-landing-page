baf39da54020b4e6054c8a2376008b2ef0ee3fdb
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2023-10-24 18:22:48 -0300

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <nav className="mx-auto p-4 bg-amber-400">
+    <div className="container mx-auto"></div>
+   </nav>
+  )
+}
+
+export default Navbar
